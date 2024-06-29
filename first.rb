d3a46fb1a922ad411d676df043ddff37ec71b594
JacinthaDev <jacinthadev8@gmail.com>
2024-06-29 09:53:41 -0400

    lesson 3 commit

---
+++ first.rb
@@ -0,0 +1,12 @@
+puts "Enter your name."
+name = gets.chomp
+puts "Well hello, #{name}!"
+
+# irb
+# example_string = "this is a string"
+# example_string.class
+# :name.class
+# this_hash = {}
+# this_hash.class
+# this_hash.methods
+# quit
