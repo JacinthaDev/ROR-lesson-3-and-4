ad30432a03b5dd8ba4828a0cdc78533dc3bf697b
Jacintha <129231721+JacinthaDev@users.noreply.github.com>
2024-07-20 09:58:30 -0400

    Change language convention of loop

--- hash_to_array.rb
+++ hash_to_array.rb
@@ -1,16 +1,14 @@
 hash = {}
-5.times {
 
-
-puts "Enter a key"
-key = gets.chomp
-
-puts "Enter a value"
-value = gets.chomp
-
-hash[key] = value
-}
-
+5.times do
+    puts "Enter a key"
+    key = gets.chomp
+    
+    puts "Enter a value"
+    value = gets.chomp
+    
+    hash[key] = value
+end
 
 def hash_method (hash)
     p hash.keys
